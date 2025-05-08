ed43731c55f5699d1ffb4e9c279d852ff7d98861
Patricia <patri.carrasco@gmail.com>
2025-05-08 12:58:36 +0200

    done chainer

---
+++ chainer.py
@@ -0,0 +1,19 @@
+
+def add10(x): return x + 10
+def mul30(x): return x * 30
+
+
+
+def chain(init_val, functions):
+  
+    for f in functions:
+       init_val= f(init_val)
+       return init_val
+
+
+
+print(chain(50, [add10, mul30]))
+
+
+
+
